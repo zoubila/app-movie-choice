8204b934a384f38659544c29c42b29230687d6ee
zoubila <manueldreano@Zoubila-002.lan>
2024-10-13 18:03:41 +0200

    security business

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -37,6 +37,7 @@ class RegistrationController extends AbstractController
 
         return $this->render('registration/register.html.twig', [
             'registrationForm' => $form,
+            'user' => $user,
         ]);
     }
 }
